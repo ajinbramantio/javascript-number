d084bfc0aa2e231944dce2c043466a57d1b9a876
M Haidar Hanif <git@mhaidarhanif.com>
2019-01-30 10:53:03 +0700

    Fix code quality

--- index.js
+++ index.js
@@ -1,64 +1,64 @@
-console.log('Javascript');
-console.log('');
+console.log(
+  '%c Javascript!',
+  'font-size: 20px; background: #222; color: #bada55'
+)
 
 // Number
-const num1 = 3;
-const num2 = 5;
-const num3 = 7;
-const num4 = 9;
-const num5 = 11;
-// End Number
+const number1 = 3
+const number2 = 5
+const number3 = 7
+const number4 = 9
+const number5 = 11
 
 // Output Number
-console.log('this is number ' + num1);
-console.log('this is number ' + num2);
-console.log('this is number ' + num3);
-console.log('this is number ' + num4);
-console.log('this is number ' + num5);
-// end output Number
-console.log('---------------------------');
+console.log('this is number ' + number1)
+console.log('this is number ' + number2)
+console.log('this is number ' + number3)
+console.log('this is number ' + number4)
+console.log('this is number ' + number5)
+console.log('--------------------------')
 
 // Addition
-const result_multiplication = num1 * num2;
-const result_divide = num4 / num1;
-const result_addition = num3 + num4;
-const result_subtraction = num5 - num4;
-const result_modulus = num3 % num2;
-const result_exponentiation = num2 ** num1;
-// End Addition
+const result_multiplication = number1 * number2
+const result_divide = number4 / number1
+const result_addition = number3 + number4
+const result_subtraction = number5 - number4
+const result_modulus = number3 % number2
+const result_exponentiation = number2 ** number1
 
 // Output Addition
-console.log(`${num1} multiplied ${num2} equals to ${result_multiplication}`);
-console.log(`${num4} divided ${num1} equals to ${result_divide}`);
-console.log(`${num3} added ${num4} equals to ${result_addition}`);
-console.log(`${num5} substracted ${num4} equals to ${result_subtraction}`);
-console.log(`${num3} modulus ${num2} equals to ${result_modulus}`);
 console.log(
-  `${num2} exponentiation ${num1} equals to ${result_exponentiation}`
-);
-
-// End Out Addition
-console.log('---------------------------');
+  `${number1} multiplied by ${number2} equals to ${result_multiplication}`
+)
+console.log(`${number4} divided by ${number1} equals to ${result_divide}`)
+console.log(`${number3} added by ${number4} equals to ${result_addition}`)
+console.log(
+  `${number5} substracted by ${number4} equals to ${result_subtraction}`
+)
+console.log(`${number3} modulus by ${number2} equals to ${result_modulus}`)
+console.log(
+  `${number2} exponentiation by ${number1} equals to ${result_exponentiation}`
+)
+console.log('---------------------------')
 
 // String
-const string1 = 'Hello';
-const string2 = 'coders';
-const string3 = 'welcome';
-const string4 = 'to';
-const string5 = 'the';
-const string6 = 'world';
-const string7 = 'of';
-const string8 = 'coding';
+const string1 = 'Hello'
+const string2 = 'coders'
+const string3 = 'welcome'
+const string4 = 'to'
+const string5 = 'the'
+const string6 = 'world'
+const string7 = 'of'
+const string8 = 'coding'
 
-const result_string = `${string1} ${string2} ${string3} ${string4} ${string5} ${string6} ${string7} ${string8}`;
-//End String
-//Output String
-console.log(result_string);
+const result_string = `${string1} ${string2} ${string3} ${string4} ${string5} ${string6} ${string7} ${string8}`
 
-console.log('---------------------------');
+// Output String
+console.log(result_string)
+console.log('---------------------------')
 
 // Array
-var arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var arrayNumber = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 var arrayString = [
   'one',
   'two',
@@ -69,13 +69,13 @@ var arrayString = [
   'seven',
   'eight',
   'nine'
-];
+]
 
-var date = new Date();
+var date = new Date()
 
 var stroller = [
   {
-    merk: 'Pliko',
+    brand: 'Pliko',
     price: 650000,
     color: 'Red',
     stock: 6,
@@ -83,7 +83,7 @@ var stroller = [
     dated: `${date.getDate()} - ${date.getMonth() + 1} - ${date.getFullYear()}`
   },
   {
-    merk: 'Does',
+    brand: 'Does',
     price: 825000,
     color: 'Brown',
     stock: null,
@@ -91,40 +91,40 @@ var stroller = [
     dated: `${date.getDate()} - ${date.getMonth() + 1} - ${date.getFullYear()}`
   },
   {
-    merk: 'Elle',
+    brand: 'Elle',
     price: 1250000,
     color: 'Blue',
     stock: 12,
     boolean: true,
     dated: `${date.getDate()} - ${date.getMonth() + 1} - ${date.getFullYear()}`
   }
-];
+]
 
-arrayNumber.push(10);
-//End Array
+arrayNumber.push(10)
 
 // Output Array
 console.log(
   `values arrayNumber is ${arrayNumber}, number of lengths ${
     arrayNumber.length
   }`
-);
+)
 
-arrayString.push('ten');
+arrayString.push('ten')
+
 console.log(
   `values arrayNumber is ${arrayString}, number of lengths ${
     arrayString.length
   }`
-);
+)
 
 stroller.push({
-  merk: 'Bugaboo',
+  brand: 'Bugaboo',
   price: 1500000,
   color: 'Green',
   stock: 10,
   boolean: true,
   dated: `${date.getDate()} - ${date.getMonth() + 1} - ${date.getFullYear()}`
-});
-console.log(stroller);
-console.log(stroller[stroller.length - 1].merk + ' is a new Merk');
-//End Output Array
+})
+
+console.log(stroller)
+console.log(stroller[stroller.length - 1].brand + ' is a new brand')
